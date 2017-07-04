5dd34f4f459fc6e8667ff807ace8a7265da0d99f
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2017-07-04 21:27:46 +0200

    Refactoring app.php

--- web/app.php
+++ web/app.php
@@ -1,12 +1,20 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
+$env = getenv('ENVIRONMENT');
+if ($env === 'dev') {
+    Debug::enable();
+} else {
+    $env = 'prod';
+}
+
+$kernel = new AppKernel($env, false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
