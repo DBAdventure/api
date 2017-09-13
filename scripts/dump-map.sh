23ad7224f0f50e76e06ff80d10449cba915b8fc3
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2017-09-13 22:39:02 +0200

    Add missing table

--- scripts/dump-map.sh
+++ scripts/dump-map.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pg_dump --data-only --inserts -t map -t map_bonus -t map_image -t map_image_file -U dba -W -h127.0.0.1 dba > ../api/var/sql/map.sql
+pg_dump --data-only --inserts -t map -t map_bonus -t map_box -t map_image -t map_image_file -U dba -W -h127.0.0.1 dba > ../api/var/sql/map.sql
